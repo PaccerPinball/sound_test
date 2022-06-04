62ac2332554b66883526016f0cbcfe071eaec3bc
Krxwallo <ken.grill@gmx.de>
2022-06-04 14:05:04 +0200

    :tada: Add sound_test.ino for testing the PaccerSound library

---
+++ sound_test.ino
@@ -0,0 +1,43 @@
+#include <PaccerSound.h>
+
+#define PIN 8 // speaker pin
+#define TEMPO 105
+
+PaccerSound sound(PIN, TEMPO);
+
+/*
+ * notes of the melody followed by the duration
+ * a 4 means a quarter note, 8 an eighteenth , 16 sixteenth, so on
+ * !!negative numbers are used to represent dotted notes,
+ * so -4 means a dotted quarter note, that is, a quarter plus an eighteenth!!
+ */
+int testMelody[] = {
+  NOTE_B4, 16, NOTE_B5, 16, NOTE_FS5, 16, NOTE_DS5, 16, //1
+  NOTE_B5, 32, NOTE_FS5, -16, NOTE_DS5, 8, NOTE_C5, 16,
+  NOTE_C6, 16, NOTE_G6, 16, NOTE_E6, 16, NOTE_C6, 32, NOTE_G6, -16, NOTE_E6, 8,
+
+  NOTE_B4, 16,  NOTE_B5, 16,  NOTE_FS5, 16,   NOTE_DS5, 16,  NOTE_B5, 32,  //2
+  NOTE_FS5, -16, NOTE_DS5, 8,  NOTE_DS5, 32, NOTE_E5, 32,  NOTE_F5, 32,
+  NOTE_F5, 32,  NOTE_FS5, 32,  NOTE_G5, 32,  NOTE_G5, 32, NOTE_GS5, 32,  NOTE_A5, 16, NOTE_B5, 8,
+
+  MELODY_END
+};
+
+
+unsigned long startMillis;
+
+void setup() {
+  Serial.begin(115200);
+  sound.melody(testMelody);
+  startMillis = millis();
+}
+
+void loop() {
+  if (millis() > startMillis + 10000) {
+    // Replay every 10s
+    sound.melody(testMelody);
+    startMillis = millis();
+  }
+  
+  sound.tick(); // has to be called every loop()
+}
